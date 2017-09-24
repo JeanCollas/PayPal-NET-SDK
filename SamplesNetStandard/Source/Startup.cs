f9cb9d0de144f446cab8d9913e6d4e535b1c7f18
Jean Collas <JeanCollas@users.noreply.github.com>
2017-09-24 20:14:41 +0200

    Format

--- SamplesNetStandard/Source/Startup.cs
+++ SamplesNetStandard/Source/Startup.cs
@@ -1,45 +1,45 @@
-public class Startup 
+public class Startup
 {
-        public Startup(IHostingEnvironment env)
+    public Startup(IHostingEnvironment env)
+    {
+        CurrentEnvironment = env;
+    }
+    private IHostingEnvironment CurrentEnvironment { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddTransient<PayPalService, PayPalService>();
+        IConfigurationSection ppSection;
+        if (CurrentEnvironment.IsDevelopment())
         {
-            CurrentEnvironment = env;
+            ppSection = Configuration.GetSection("Dev:PayPal");
         }
-        private IHostingEnvironment CurrentEnvironment { get; set; }
+        else
+        { // Prod
+            ppSection = Configuration.GetSection("PayPal");
+        }
+        var ppConf = new PaypalConfig();
+        ppSection.Bind(ppConf);
+        // method 1
+        services.Configure<PaypalConfig>(o => ppSection.Bind(o));
+        // method 2
+        PayPal.Api.ConfigManager.Instance.SetConfig(ppConf.Mode, ppConf.ClientId, ppConf.ClientSecret);
 
-        public void ConfigureServices(IServiceCollection services)
-        {
 
-			services.AddTransient<PayPalService, PayPalService>();
-			IConfigurationSection ppSection;
-			if (CurrentEnvironment.IsDevelopment())
-			{
-				ppSection = Configuration.GetSection("Dev:PayPal");
-			}
-			else
-			{ // Prod
-				ppSection = Configuration.GetSection("PayPal");
-			}
-			var ppConf = new PaypalConfig();
-			ppSection.Bind(ppConf);
-			// method 1
-			services.Configure<PaypalConfig>(o => ppSection.Bind(o));
-			// method 2
-			PayPal.Api.ConfigManager.Instance.SetConfig(ppConf.Mode, ppConf.ClientId, ppConf.ClientSecret);
-			
+        // dev password policy
+        PasswordOptions po = new PasswordOptions() { RequireDigit = false, RequiredLength = 4, RequireLowercase = false, RequireNonAlphanumeric = false, RequireUppercase = false };
 
-			// dev password policy
-            PasswordOptions po = new PasswordOptions() { RequireDigit = false, RequiredLength = 4, RequireLowercase = false, RequireNonAlphanumeric = false, RequireUppercase = false };
-			
-			services.AddIdentity<AppUser, AppRole>(e => { e.Password = po; })
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
+        services.AddIdentity<AppUser, AppRole>(e => { e.Password = po; })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
 
-			services.AddAuthentication();
+        services.AddAuthentication();
 
-		}
-		
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            app.UseAuthentication();
-		}
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+        app.UseAuthentication();
+    }
 }
